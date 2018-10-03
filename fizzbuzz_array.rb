ece78b1b0d62fb0c1d46e5661a5acd90ed1ac076
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-10-03 19:05:18 -0400

    Question to why I have to use a function to test my array but use a variable to get it to print

--- fizzbuzz_array.rb
+++ fizzbuzz_array.rb
@@ -2,6 +2,7 @@ def fizz_array
   fizz_array = []
 end
 
+
 def buzz(number, fizz_array)
   if number % 15 == 0
     fizz_array<< "mined"
@@ -10,16 +11,18 @@ def buzz(number, fizz_array)
   elsif number % 3 == 0
     fizz_array<< "mined minds"
   else
+    # print fizz array
     fizz_array<< number
   end
 end
 
 number = 1
+fizz_array = []
 
 until number == 101
   buzz(number, fizz_array)
   number += 1
   if number == 101
-    print fizz_array
+  print  fizz_array[0..100]
   end
 end
